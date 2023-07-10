4a4c1f3791386314c1aeca75e6d8a692b92dcdde
lawrence <lawrence@furence.com>
2023-07-10 14:06:40 +0900

    커밋 테스트

--- src/javaStudy0703/AB.java
+++ src/javaStudy0703/AB.java
@@ -6,8 +6,10 @@ public class AB {
 
 	public static void printAB(String[] args) {
 		Scanner scan = new Scanner(System.in);
+		
 		double a = scan.nextDouble();
 		double b = scan.nextDouble();
+		
 		System.out.println(a/b);
 	}
 

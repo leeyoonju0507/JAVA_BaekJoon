b2c4d6024d8f8735022b9ef5fc0398c37e9c36b8
lawrence <lawrence@furence.com>
2023-07-12 16:15:40 +0900

    ABPLUS 쌤 풀이 추가

--- src/javaStudy0710/ABPLUS.java
+++ src/javaStudy0710/ABPLUS.java
@@ -1,5 +1,8 @@
 package javaStudy0710;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class ABPLUS {
@@ -7,23 +10,37 @@ public class ABPLUS {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		String a = scan.next();
-		
-		if(a.length()==4) {
-			System.out.println(20);
-		}
-		else if(a.charAt(1)=='0' && a.length()==3) {
-			System.out.println((int)a.charAt(2)-48+10);
-			
-		}
-		else if(a.charAt(2)=='0' && a.length()==3) {
-			System.out.println((int)a.charAt(0)+10-48);
-			
-		}else {
-			System.out.println((int)a.charAt(0)+(int)a.charAt(1)-96);
-			
-		}
-		
-		
-	}
 
+		if (a.length() == 4) {
+			System.out.println(20);
+		} else if (a.charAt(1) == '0' && a.length() == 3) {
+			System.out.println((int) a.charAt(2) - 48 + 10);
+
+		} else if (a.charAt(2) == '0' && a.length() == 3) {
+			System.out.println((int) a.charAt(0) + 10 - 48);
+
+		} else {
+			System.out.println((int) a.charAt(0) + (int) a.charAt(1) - 96);
+
+		}
+
+	}
+	
+	// 쌤 풀이
+	public void teacher() throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String in = br.readLine();
+		if("1010".equals(in)) {
+			System.out.println(20);
+		}else if(in.contains("10")) {
+			in = in.replace("10","");
+			System.out.println(Integer.parseInt(in) + 10);
+		}else {
+			int a = Integer.parseInt(in);
+			System.out.println((a/10)+(a%10));			
+		}
+	}
 }
+
+	
+
